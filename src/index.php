024852c5407ae414e98ff6f65f1ccabd7df426b2
Amabile <amabile.kosloski@gmail.com>
2025-09-01 00:30:02 -0300

    feat: adicionando botões

--- src/index.php
+++ src/index.php
@@ -13,7 +13,52 @@ include "database/db.php";
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
   </head>
   <body>
-    <h1>Hello, world!</h1>
+
+  <?php include ('view/navbar.php'); ?>
+  <div class="container mt-4">
+    <div class="row">
+      <div class="col-md-12">
+        <div class="card">
+          <div class="card-header">
+            <h4> Lista de Usuários
+             <a href="create_action.php" class="btn btn-primary float-end"> Adicionar Usuário</a>
+            </h4>
+          </div>
+          <div class="card-body">
+            <table class="table table-bordered table-striped">
+              <thead>
+                <tr>
+                  <th>ID</th>
+                  <th>Nome</th>
+                  <th>EMail</th>
+                  <th>Data de Nascimento</th>
+                  <th>Ações</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>1</td>
+                  <td>Amábile Kosloski</td>
+                  <td>amabile.kosloski@gmail.com</td>
+                  <td>19/10/2004</td>
+                  <td>
+                    <a href="" class="btn btn-secondary btn-sm">Visualizar</a>
+                    <a href="" class="btn btn-success btn-sm">Editar</a>
+                    <form action="" method="POST" class="d-inline">
+                      <button type="submit" name="delete_usuario" value="1" class="btn btn-danger btn-sm">
+                        Excluir
+                      </button>
+                    </form>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
   </body>
 </html>
